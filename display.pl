89c636fe75e793e5ef576b690d6f6d4264a71ed4
Markuze Alex <markuze@cs.technion.ac.il>
2019-03-04 17:11:27 +0200

    fisplay.pl: minor fixes

--- display.pl
+++ display.pl
@@ -17,6 +17,7 @@ usage() unless (defined($opts{'d'}));
 
 my %hash = ();
 my @header = ();
+my %tests = ();
 my %options = ();
 
 sub parse_csv {
@@ -57,6 +58,8 @@ foreach (@header) {
 }
 
 $idnt = 0;
+printf "\noptions: \n";
+
 foreach (sort keys %options) {
 	printf "$_ ";
 	$idnt++;
@@ -64,29 +67,35 @@ foreach (sort keys %options) {
 		printf "\n";
 	}
 }
+
+#TODO: Read from cfg file
 my @keys = qw(
-	sys_Memory cpu_total sys_Read sys_Write Total_rx_bytes Total_tx_bytes 
+	sys_Memory cpu_total sys_Read sys_Write Total_rx_bytes Total_tx_bytes ddr_0  ddr_1
+	sys_total_CRd sys_total_DRd sys_total_ItoM sys_total_PCIeRd sys_total_PCIeRdCur
+	sys_total_PCIeWr sys_total_PRd sys_total_RFO sys_total_WiL
+
 );
 
 my @tests = qw(
-	tcp_bi_s1_nL_p2 tcp_bi_s1_n2_pR tcp_bi_s1_n2_pT 
+	tcp_bi_s1_nL_p2 tcp_bi_s1_n2_pR tcp_bi_s1_n2_pT
 	tcp_bi_s1_n2_p2
 );
 
 printf "\n";
-	
+
 open my $fh, '>', "setup.csv";
-printf $fh "test, setup, bandwidth ";	
+printf $fh "test, setup, bandwidth ";
 foreach my $key (@keys) {
-printf $fh ",$key";
+	printf $fh ",$key";
 }
 printf $fh "\n";
 
-foreach my $test (@tests) {
+#foreach my $test (@tests) {
+foreach my $test (sort keys %options) {
 	for my $setup (sort keys (%hash)) {
 		printf $fh "$test, $setup ";
 		printf " Undefined!!!" unless (defined($hash{$setup}{$test}));
-		printf $fh ", %.2f", $hash{$setup}{$test}{'Total_rx_bytes'} 
+		printf $fh ", %.2f", $hash{$setup}{$test}{'Total_rx_bytes'}
 					+ $hash{$setup}{$test}{'Total_tx_bytes'};
 		foreach my $key (@keys) {
 		#	next unless defined  ($hash{$setup}{$test}{$key});
